6e44ea1ed2427c4748855a271045235c94f42667
Nicolas Abril <nicolas.abril@gmail.com>
2018-11-13 11:05:52 -0200

    Termina ultrassom

--- codigo/ultrasound.py
+++ codigo/ultrasound.py
@@ -2,21 +2,30 @@
 # Informações sobre a licensa no arquivo LICENSE
 
 import RPi.GPIO as GPIO
-import time 
+import time
+import signal
 
 class Ultrasound ():
-	distance = None
+    distance = None
 
     def __init__ (self, echo_pin, trigger_pin, max_distance):
         self.echo_pin = echo_pin
         self.trigger_pin = trigger_pin
-
+        GPIO.setmode(GPIO.BOARD)
         GPIO.setup(trigger_pin, GPIO.OUT)
         GPIO.setup(echo_pin, GPIO.IN)
         GPIO.add_event_detect(echo_pin, GPIO.RISING, callback=self.startCountingEchoTime)
         GPIO.add_event_detect(echo_pin, GPIO.FALLING, callback=self.stopCountingEchoTime)
+        signal.signal(signal.SIGALRM, self.sendTrigger)
+        signal.setitimer(signnal.ITIMER_REAL, 0.1, 0.1)
+        
+    def sendTrigger (self):
+        GPIO.output(self.trigger_pin, GPIO.HIGH)
+        GPIO.output(self.trigger_pin, GPIO.LOW)
+        
     def startCountingEchoTime (self):
         self.echo_time = time.time()
+        
     def stopCountingEchoTime (self):
         self.echo_time = time.time() - self.echo_time
         self.distance = self.echo_time * 170 			# Velocidade do som (340) / 2 (tem que ir e voltar)
