0cd2bc4219999228e08321db090957d713ae863f
AlefeFelipe <alefe@alunos.utfpr.edu.br>
2018-11-13 21:11:11 -0200

    inicia classe bluetooth

---
+++ codigo/bluetooth.py
@@ -0,0 +1,14 @@
+import bluetooth
+
+class Bluetooth():
+    def __init__ ():
+        self.server_sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+        port = 1
+        server_sock.bind(("",port))
+        server_sock.listen(1)
+
+    def read():
+        client_sock,address = server_sock.accept()#sera? talvez nao caia a conexao nunca, dai fecha qd fechar o programa
+        data = client_sock.recv(1024)
+        client_sock.close()
+        server_sock.close()
