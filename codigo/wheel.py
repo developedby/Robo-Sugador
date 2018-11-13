cd959c68e9991493b61bb31c1730967cc6dc108f
AlefeFelipe <alefe@alunos.utfpr.edu.br>
2018-11-13 20:31:35 -0200

    Conserta wheel, faz dcmotor

--- codigo/wheel.py
+++ codigo/wheel.py
@@ -7,16 +7,15 @@ class Wheel (DCMotor):
         self.pwm_pin = pwm_pin
         super().__init__(inp1_pin, inp2_pin)
         self.encoder = Encoder(encoder_pin, holes_number)
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
         self.pwm = GPIO.PWM(self.pwm_pin, 60)#60hz ta bom?
         self.pwm.start(0)#inicia parado
 
     def walk(speed):
-         if(speed >= 0)
-         {
-            rotate(True)
-         }
-         else
-            rotate(False)
+         if(speed => 0):
+            rotate(1)
+        else:
+            rotate(-1)
         pulse = (speed/250)*100#tem que ver a resolucao do motor, 250 é um valor aleatorio
         self.pwm.ChangeDutyCycle(pulse)
         #tem que fazer a funcao que le a velocidade real e manda para  o movimentador
