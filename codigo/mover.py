927adc7cff907e14644862ce573b0ebe6e22aaab
AlefeFelipe <alefe@alunos.utfpr.edu.br>
2018-12-05 10:39:42 -0200

    corrige logica velocidades de novo e atualiza vel mais devagar

--- codigo/mover.py
+++ codigo/mover.py
@@ -8,15 +8,15 @@ def clamp(num, min_value, max_value):
    return max(min(num, max_value), min_value)
 
 class Mover():
-    speed_adjust_frequency = 0.1
+    speed_adjust_frequency = 0.2
     wheel_initial_forward_speed = (0, 0)
     wheel_initial_turn_speed = 50
 
     def __init__ (self, input1_a_pin, input2_a_pin, pwm_a_pin, encoder_a_pin, num_a_holes, input1_b_pin, input2_b_pin, pwm_b_pin, encoder_b_pin, num_b_holes, speed_adjust_delta):
         self.left_wheel = Wheel(input1_a_pin, input2_a_pin, pwm_a_pin, encoder_a_pin, num_a_holes, .9*self.speed_adjust_frequency)
         self.right_wheel = Wheel(input1_b_pin, input2_b_pin, pwm_b_pin, encoder_b_pin, num_b_holes, .9*self.speed_adjust_frequency)
-        self.left_wheel_sent_speed = None               # velocidade enviada a roda, em % do pwm
-        self.right_wheel_sent_speed = None
+        self.left_wheel_abs_sent_speed = None               # velocidade enviada a roda, em % do pwm
+        self.right_wheel_abs_sent_speed = None
         self.left_wheel_required_speed = None           # velocidade desejada, em graus por segundo
         self.right_wheel_required_speed = None
         self.speed_adjust_delta = speed_adjust_delta    # quanto a velocidade enviada varia a cada ajuste
@@ -27,47 +27,47 @@ class Mover():
     def adjustSpeed(self):
         if self.sign(self.left_wheel_required_speed) == self.sign(self.right_wheel_required_speed):
             if self.left_wheel_required_speed > 0:
-                self.wheel_initial_forward_speed = (self.left_wheel_sent_speed, self.right_wheel_sent_speed)
+                self.wheel_initial_forward_speed = (self.left_wheel_abs_sent_speed, self.right_wheel_abs_sent_speed)
             else:
-                self.wheel_initial_forward_speed = (self.right_wheel_sent_speed, self.left_wheel_sent_speed)
+                self.wheel_initial_forward_speed = (self.right_wheel_abs_sent_speed, self.left_wheel_abs_sent_speed)
 
         if self.left_wheel.encoder.angular_velocity > abs(self.left_wheel_required_speed):
-            self.left_wheel_sent_speed -= self.sign(self.left_wheel_required_speed)*self.speed_adjust_delta
+            self.left_wheel_abs_sent_speed -= self.speed_adjust_delta
         elif self.left_wheel.encoder.angular_velocity < abs(self.left_wheel_required_speed):
-            self.left_wheel_sent_speed += self.sign(self.left_wheel_required_speed)*self.speed_adjust_delta
+            self.left_wheel_abs_sent_speed += self.speed_adjust_delta
 
         if self.right_wheel.encoder.angular_velocity > abs(self.right_wheel_required_speed):
-            self.right_wheel_sent_speed -= self.sign(self.right_wheel_required_speed)*self.speed_adjust_delta
+            self.right_wheel_abs_sent_speed -= self.speed_adjust_delta
         elif self.right_wheel.encoder.angular_velocity < abs(self.right_wheel_required_speed):
-            self.right_wheel_sent_speed += self.sign(self.right_wheel_required_speed)*self.speed_adjust_delta
+            self.right_wheel_abs_sent_speed += self.speed_adjust_delta
 
-        self.left_wheel_sent_speed = self.sign(self.left_wheel_required_speed)*clamp(self.left_wheel_sent_speed, 0, 99)
-        self.right_wheel_sent_speed = self.sign(self.right_wheel_required_speed)*clamp(self.right_wheel_sent_speed, 0, 99)
-        self.left_wheel.spin(self.left_wheel_sent_speed)
-        self.right_wheel.spin(self.right_wheel_sent_speed)
+        self.left_wheel_abs_sent_speed = clamp(self.left_wheel_abs_sent_speed, 0, 99)
+        self.right_wheel_abs_sent_speed = clamp(self.right_wheel_abs_sent_speed, 0, 99)
+        self.left_wheel.spin(self.sign(self.left_wheel_required_speed)*self.left_wheel_abs_sent_speed)
+        self.right_wheel.spin(self.sign(self.right_wheel_required_speed)*self.right_wheel_abs_sent_speed)
 
         print("Vel desejada roda direita:", self.right_wheel_required_speed)
         print("Vel lida na roda direita:", self.sign(self.right_wheel_required_speed) * self.right_wheel.encoder.angular_velocity)
-        print("Escrevendo roda direita:", self.right_wheel_sent_speed)
+        print("Escrevendo roda direita:", self.sign(self.right_wheel_required_speed)*self.right_wheel_abs_sent_speed)
 
         print("Vel desejada roda esquerda:", self.left_wheel_required_speed)
         print("Vel lida na roda esquerda:", self.sign(self.left_wheel_required_speed) * self.left_wheel.encoder.angular_velocity)
-        print("Escrevendo roda esquerda:", self.left_wheel_sent_speed)
+        print("Escrevendo roda esquerda:", self.sign(self.left_wheel_required_speed)*self.left_wheel_abs_sent_speed)
 
         self.setTimer()
 
     def moveForward(self, speed):
         if speed > 0:
-            self.left_wheel_sent_speed = self.sign(speed)*self.wheel_initial_forward_speed[0]
-            self.right_wheel_sent_speed = self.sign(speed)*self.wheel_initial_forward_speed[1]
+            self.left_wheel_abs_sent_speed = self.wheel_initial_forward_speed[0]
+            self.right_wheel_abs_sent_speed = self.wheel_initial_forward_speed[1]
         else:
-            self.left_wheel_sent_speed = self.sign(speed)*self.wheel_initial_forward_speed[1]
-            self.right_wheel_sent_speed = self.sign(speed)*self.wheel_initial_forward_speed[0]
+            self.left_wheel_abs_sent_speed = self.wheel_initial_forward_speed[1]
+            self.right_wheel_abs_sent_speed = self.wheel_initial_forward_speed[0]
 
         self.left_wheel_required_speed = speed
         self.right_wheel_required_speed = speed
-        self.left_wheel.spin(self.left_wheel_sent_speed)
-        self.right_wheel.spin(self.right_wheel_sent_speed)
+        self.left_wheel.spin(self.left_wheel_abs_sent_speed)
+        self.right_wheel.spin(self.right_wheel_abs_sent_speed)
 
         print("Andando pra frente com vel", speed)
 
@@ -77,8 +77,8 @@ class Mover():
     def turn(self, speed): # usando sentido positivo: antihorario
         self.left_wheel_required_speed = -speed
         self.right_wheel_required_speed = speed
-        self.left_wheel_sent_speed = -self.sign(speed)*self.wheel_initial_turn_speed
-        self.right_wheel_sent_speed = self.sign(speed)*self.wheel_initial_turn_speed
+        self.left_wheel_abs_sent_speed = self.wheel_initial_turn_speed
+        self.right_wheel_abs_sent_speed = self.wheel_initial_turn_speed
 
         print("Girando com vel", speed)
 
@@ -89,8 +89,8 @@ class Mover():
         self.stopTimer()
         self.left_wheel.spin(0)
         self.right_wheel.spin(0)
-        self.left_wheel_sent_speed = 0
-        self.right_wheel_sent_speed = 0
+        self.left_wheel_abs_sent_speed = 0
+        self.right_wheel_abs_sent_speed = 0
         self.left_wheel_required_speed = 0
         self.right_wheel_required_speed = 0
         print("Parando rodas")
